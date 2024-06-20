dd9f573e93238ec8a6076d0a547605b7dd0bece2
Bhupendra Rajput <bjrajput15800@gmail.com>
2024-06-20 13:48:57 +0530

    updated

--- frontend/src/components/Feedback/FeedbackForm.js
+++ frontend/src/components/Feedback/FeedbackForm.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const FeedbackForm = () => {
-  const [category, setCategory] = useState('Product Features');
+  const [category, setCategory] = useState('');
   const [rating, setRating] = useState(1);
   const [comments, setComments] = useState('');
   const { getAccessTokenSilently } = useAuth0();
@@ -24,25 +24,29 @@ const FeedbackForm = () => {
   };
 
   return (
-    <div>
-      <h2>Submit Feedback</h2>
-      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', rowGap: '5vh' }}>
+      <h2>Feedback Form</h2>
+      <select value={category} onChange={(e) => setCategory(e.target.value)} style={{ width: '30vw', height: '6vh' }}>
         <option value="Product Features">Product Features</option>
         <option value="Product Pricing">Product Pricing</option>
         <option value="Product Usability">Product Usability</option>
       </select>
       <input
         type="number"
+        placeholder='Enter rating'
         value={rating}
         onChange={(e) => setRating(e.target.value)}
         min="1"
         max="5"
+        style={{ width: '30vw', height: '6vh' }}
       />
       <textarea
+        placeholder='Enter Comments'
         value={comments}
         onChange={(e) => setComments(e.target.value)}
+        style={{ width: '30vw', height: '6vh' }}
       />
-      <button onClick={submitFeedback}>Submit</button>
+      <button onClick={submitFeedback} style={{ width: '30vw', height: '6vh' }}>Submit</button>
     </div>
   );
 };
